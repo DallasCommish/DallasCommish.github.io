b637e8dbb2fe948ca4a8e19e1a5d94fc5359566b
DallasCommish <dmcnick150@gmail.com>
2024-09-30 08:18:45 -0700

    2024 AL finish and draft order updated

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,3 +1,54 @@
+function showFinish2024() {
+    var teamOrderAL = ["The Shohei Kid", "Kirk's Enterprise", "Wichita TinMen",
+                       "Jak of Diamonds", "Banoaster Republic", "Flower Children",
+                       "David's 5 & Dime", "Hradek Hrams", "The Patsy Killjoys",
+                       "The Blossoms", "Non-Zero Chance", "Kelly S*M*A*S*H"];
+    var teamPointsAL = [105, 99, 87, 77, 69, 68, 58, 56, 43, 41, 40, 37];
+    var teamOrderNL = ["Wichita Sox Monkees", "Matty Bos", "Leonard Peltier's Army",
+                       "Donner Party", "Dread Pirate Roberts", "Seattle Stackers",
+                       "P-Funk All-Stars", "A McNickle for Farm Fresh Gregs", "Liberty Lunch",
+                       "The Cream and the Clear", "NWA Redbirds", ''];
+    var teamPointsNL = [72, 71, 61, 58, 54, 53, 46, 36, 30, 26, 11, 0];
+  
+    var htmlCode = "<table id='BaseballFinish' class='responsive'><thead><th colspan='5'>2024 Finish</th></thead>";
+    htmlCode += "<tr><td class='colTitle'>American League</td>";
+    htmlCode += "<td class='colTitle'>Points</td>";
+    htmlCode += "<td class='colTitle'>Place</td>";
+    htmlCode += "<td class='colTitle'>National League</td>";
+    htmlCode += "<td class='colTitle'>Points</td></tr><tbody>";
+  
+    for (var i = 0; i < 12; i++) {
+      htmlCode += "<tr><td class='team'>" + teamOrderAL[i] + "</td>";
+      htmlCode += "<td class='points'>" + teamPointsAL[i].toFixed(1) + "</td>";
+      htmlCode += "<td class='points'>" + (i + 1) + "</td>";
+      htmlCode += "<td class='team'>" + teamOrderNL[i] + "</td>";
+      htmlCode += "<td class='points'>" + teamPointsNL[i].toFixed(1) + "</td></tr>";
+    }
+  
+    htmlCode += "</tbody></table>";
+    htmlCode += "<p>The Shohei Kid wins the tie-breaker 8,292 - 7,829";
+
+    htmlCode += "<table id='prizePair'>";
+    htmlCode += "<caption>Final Prize Pair Selections</caption>";
+    htmlCode += "<thead>";
+    htmlCode += "<td>Prize Money</td><td>Draft Order</td><td>Owner</td>";
+    htmlCode += "</thead>";
+    htmlCode += "<tr><td>50%</td><td>11<sup>th</sup></td><td>Glenn</td></tr>";
+    htmlCode += "<tr><td>25%</td><td>10<sup>th</sup></td><td>Rich</td></tr>";
+    htmlCode += "<tr><td>15%</td><td>9<sup>th</sup></td><td>Matt R</td></tr>";
+    htmlCode += "<tr><td>10%</td><td>8<sup>th</sup></td><td>David</td></tr>";
+    htmlCode += "<tr><td>$29.4</td><td>7<sup>th</sup></td><td>Doug</td></tr>";
+    htmlCode += "<tr><td>$0</td><td>1<sup>st</sup></td><td>Matt A</td></tr>";
+    htmlCode += "<tr><td>$0</td><td>2<sup>nd</sup></td><td>Jon</td></tr>";
+    htmlCode += "<tr><td>$0</td><td>3<sup>rd</sup></td><td>Marc</td></tr>";
+    htmlCode += "<tr><td>$0</td><td>4<sup>th</sup></td><td>Greg</td></tr>";
+    htmlCode += "<tr><td>$0</td><td>5<sup>th</sup></td><td>Ken</td></tr>";
+    htmlCode += "<tr><td>$0</td><td>6<sup>th</sup></td><td>Tom</td></tr>";
+    htmlCode += "</table>";
+
+    return htmlCode;
+  }
+  
 function showFinish2023() {
     var teamOrderAL = ["Banoaster Republic", "Flower Children", "Wichita TinMen",
                        "David's 5 & Dime", "The Patsy Killjoys", "Hradek Hrams",
@@ -1844,46 +1895,46 @@ function ALMinors25() {
     var pick = [];
     var superScript = [];
     // first round
-    pick.push("Glenn", "Brent", "Jordan", 
-              "Chris", "Jordan", "Brent", 
-              "Rich", "Kelly", "Marty", 
-              "Jon", "Brent", "Chris");
-    superScript.push("0","4","6",
-                     "0","0","2",
+    pick.push("Rich", "Brent", "Brent", 
+              "Jon", "Marty", "Chris", 
+              "Kelly", "Jordan", "Brent", 
+              "Glenn", "Chris", "Jordan");
+    superScript.push("0","2","0",
                      "0","0","0",
-                     "0","0","3 and 7");
+                     "0","0","4",
+                     "0","3 and 7","6");
 
     // second round
-    pick.push("Greg", "Jeremy", "Greg", 
-              "Chris", "Kelly", "Jon", 
-              "Rich", "Kelly", "Marty", 
-              "Jon", "Brent", "Randall");
-    superScript.push("8","0","0",
-                     "0","5","1",
+    pick.push("Rich", "Jon", "Brent", 
+              "Jon", "Marty", "Chris", 
+              "Kelly", "Kelly", "Jeremy", 
+              "Greg", "Randall", "Greg");
+    superScript.push("0","1","0",
                      "0","0","0",
-                     "0","0","0");
+                     "0","5","0",
+                     "8","0","0");
 
     // third round
-    pick.push("Glenn", "Jeremy", "Greg", 
-              "Chris", "Jordan", "David M", 
-              "Rich", "Kelly", "Marty", 
-              "Jon", "Brent", "Randall");
+    pick.push("Rich", "David", "Brent", 
+              "Jon", "Marty", "Chris", 
+              "Kelly", "Jordan", "Jeremy", 
+              "Glenn", "Randall", "Greg");
     superScript.push("0","0","0",
                      "0","0","0",
                      "0","0","0",
                      "0","0","0");
 
     // fourth round
-    pick.push("Glenn", "Jeremy", "Greg", 
-              "Chris", "Jordan", "David M", 
-              "Rich", "Kelly", "Marty", 
-              "Jon", "Brent", "Randall");
+    pick.push("Rich", "David", "Brent", 
+              "Jon", "Marty", "Chris", 
+              "Kelly", "Jordan", "Jeremy", 
+              "Glenn", "Randall", "Greg");
     superScript.push("0","0","0",
                      "0","0","0",
                      "0","0","0",
                      "0","0","0");
 
-    htmlCode = '<table class="responsive"><thead><th colspan="5">2025 American League Minors Roster Draft (Unordered)</th></thead>';
+    htmlCode = '<table class="responsive"><thead><th colspan="5">2025 American League Minors Roster Draft (Ordered)</th></thead>';
     htmlCode += '<tr><td class="colTitle">Slot</td><td class="colTitle">Round 1</td><td class="colTitle">Round 2</td><td class="colTitle">Round 3</td><td class="colTitle">Round 4</td></tr>';
 
     for (i = 0; i <12; i++) {
@@ -1941,48 +1992,48 @@ function ALReserves25() {
     var pick = [];
     var superScript = [];
     // first round
-    pick.push("Brent", "Marty", "Jeremy", "Randall", "Kelly", "Greg",
-        "Chris", "Jon", "David", "Glenn", "Rich", "Jordan");
-        superScript.push("0","0","0","0","0","0",
-                         "0","0","0","0","0","0");
+    pick.push("Rich", "David", "Brent", "Jon", "Marty", "Chris",
+              "Kelly", "Jordan", "Jeremy", "Glenn", "Randall", "Greg");
+    superScript.push("0","0","0","0","0","0",
+                     "0","0","0","0","0","0");
     
     // second round
-    pick.push("Brent", "Marty", "Jeremy", "Randall", "Kelly", "Greg",
-        "Chris", "Greg", "David", "Glenn", "Rich", "Jordan");
-        superScript.push("0","0","0","0","0","0",
-                         "0","1","0","0","0","0");
+    pick.push("Rich", "David", "Brent", "Greg", "Marty", "Chris",
+             "Kelly", "Jordan", "Jeremy", "Glenn", "Randall", "Greg");
+    superScript.push("0","0","0","1","0","0",
+                     "0","0","0","0","0","0");
     
     // third round
-    pick.push("Brent", "Marty", "Jeremy", "Randall", "Kelly", "Jon",
-        "Chris", "Jon", "David", "Glenn", "Rich", "Jordan");
-        superScript.push("0","0","0","0","0","1",
-                         "0","0","0","0","0","0");
+    pick.push("Rich", "David", "Brent", "Jon", "Marty", "Chris",
+              "Kelly", "Jordan", "Jeremy", "Glenn", "Randall", "Jon");
+    superScript.push("0","0","0","0","0","0",
+                     "0","0","0","0","0","1");
     
     // fourth round
-    pick.push("Brent", "Marty", "Jeremy", "Randall", "Kelly", "Greg",
-    "Chris", "Jon", "David", "Glenn", "Rich", "Jordan");
+    pick.push("Rich", "David", "Brent", "Jon", "Marty", "Chris",
+              "Kelly", "Jordan", "Jeremy", "Glenn", "Randall", "Greg");
     superScript.push("0","0","0","0","0","0",
                      "0","0","0","0","0","0");
 
     // fifth round
-    pick.push("Brent", "Marty", "Jeremy", "Randall", "Kelly", "Greg",
-    "Chris", "Jon", "David", "Glenn", "Rich", "Jordan");
+    pick.push("Rich", "David", "Brent", "Jon", "Marty", "Chris",
+              "Kelly", "Jordan", "Jeremy", "Glenn", "Randall", "Greg");
     superScript.push("0","0","0","0","0","0",
                      "0","0","0","0","0","0");
 
     // sixth round
-    pick.push("Brent", "Marty", "Jeremy", "Randall", "Kelly", "Greg",
-    "Chris", "Jon", "David", "Glenn", "Rich", "Jordan");
+    pick.push("Rich", "David", "Brent", "Jon", "Marty", "Chris",
+              "Kelly", "Jordan", "Jeremy", "Glenn", "Randall", "Greg");
     superScript.push("0","0","0","0","0","0",
                      "0","0","0","0","0","0");
 
     // seventh round
-    pick.push("Brent", "Marty", "Jeremy", "Randall", "Kelly", "Greg",
-    "Chris", "Jon", "David", "Glenn", "Rich", "Jordan");
+    pick.push("Rich", "David", "Brent", "Jon", "Marty", "Chris",
+              "Kelly", "Jordan", "Jeremy", "Glenn", "Randall", "Greg");
     superScript.push("0","0","0","0","0","0",
                      "0","0","0","0","0","0");
 
-    htmlCode = '<table class="responsive"><thead><th colspan="8">2025 American League Reserves Roster Draft (Un-ordered)</th></thead>';
+    htmlCode = '<table class="responsive"><thead><th colspan="8">2025 American League Reserves Roster Draft (Ordered)</th></thead>';
     htmlCode += '<tr><td class="colTitle">Slot</td><td class="colTitle">Round 1</td><td class="colTitle">Round 2</td><td class="colTitle">Round 3</td><td class="colTitle">Round 4</td><td class="colTitle">Round 5</td><td class="colTitle">Round 6</td><td class="colTitle">Round 7</td></tr>';
 
     for (i = 0; i <12; i++) {
