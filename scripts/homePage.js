7955031d43545e42c2c4466794e7bd15106d5db7
DallasCommish <dmcnick150@gmail.com>
2025-04-05 11:21:13 -0700

    April 4 trades entered

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1982,11 +1982,11 @@ function ALMinors27() {
     pick.push("Glenn", "Jeremy", "Greg", 
               "Chris", "Jordan", "Brent", 
               "Rich", "Kelly", "Marty", 
-              "Jon", "Brent", "Randall");
+              "Jon", "Brent", "Greg");
     superScript.push("0","0","0",
                      "0","0","1",
                      "0","0","0",
-                     "0","0","0");
+                     "0","0","2");
 
     // second round
     pick.push("Glenn", "Jeremy", "Greg", 
@@ -2044,6 +2044,84 @@ function ALMinors27() {
     // 1
     htmlCode += '<li>3/28/2024: <strong>Brent</strong> trades a 2nd round 2024 ML pick to ';
     htmlCode += '<strong>David</strong> for a 2027 1st round ML pick.</li>';
+    // 2
+    htmlCode += "<li>4/4/2025: <strong>Greg</strong> trades <em>Jazz Chisholm</em> and a ";
+    htmlCode += "2028 1st round ML pick to <strong>Randall</strong> for <em>Cam Smith</em>, ";
+    htmlCode += "<em>Juan Brito</em>, and a 2027 1st round ML pick.</li>";
+    htmlCode += '</ol>';
+    return htmlCode;
+}
+
+function ALMinors28() {
+    var pick = [];
+    var superScript = [];
+    // first round
+    pick.push("Glenn", "Jeremy", "Randall", 
+              "Chris", "Jordan", "David", 
+              "Rich", "Kelly", "Marty", 
+              "Jon", "Brent", "Randall");
+    superScript.push("0","0","2",
+                     "0","0","0",
+                     "0","0","0",
+                     "0","0","0");
+
+    // second round
+    pick.push("Glenn", "Jeremy", "Greg", 
+              "Chris", "Jordan", "David M", 
+              "Rich", "Kelly", "Marty", 
+              "Jon", "Brent", "Randall");
+    superScript.push("0","0","0",
+                     "0","0","0",
+                     "0","0","0",
+                     "0","0","0");
+
+    // third round
+    pick.push("Glenn", "Jeremy", "Greg", 
+              "Chris", "Jordan", "David M", 
+              "Rich", "Kelly", "Marty", 
+              "Jon", "Brent", "Randall");
+    superScript.push("0","0","0",
+                     "0","0","0",
+                     "0","0","0",
+                     "0","0","0");
+
+    // fourth round
+    pick.push("Glenn", "Jeremy", "Greg", 
+              "Chris", "Jordan", "David M", 
+              "Rich", "Kelly", "Marty", 
+              "Jon", "Brent", "Randall");
+    superScript.push("0","0","0",
+                     "0","0","0",
+                     "0","0","0",
+                     "0","0","0");
+
+    htmlCode = '<table class="responsive"><thead><th colspan="5">2028 American League Minors Roster Draft (Unordered)</th></thead>';
+    htmlCode += '<tr><td class="colTitle">Slot</td><td class="colTitle">Round 1</td><td class="colTitle">Round 2</td><td class="colTitle">Round 3</td><td class="colTitle">Round 4</td></tr>';
+
+    for (i = 0; i <12; i++) {
+        htmlCode += '<tr><td class="points">';
+        htmlCode += (i + 1).toString();
+        htmlCode += '</td>';
+        for (j = 0; j < 4; j++) {
+            htmlCode += '<td class="team">';
+            htmlCode += pick[j * 12 + i];
+            if (superScript[j * 12 + i] != "0") {
+                htmlCode += '<sup>' + superScript[j * 12 + i] + '</sup>';
+            }
+            htmlCode += '</td>';
+        }
+        htmlCode += '</tr>';
+    }
+
+    htmlCode += '</table>';
+
+    // include the footnotes of the trade details
+    htmlCode += '<br /><h3>Trade Details</h3>';
+    htmlCode += '<ol>';
+    // 1
+    htmlCode += "<li>4/4/2025: <strong>Greg</strong> trades <em>Jazz Chisholm</em> and a ";
+    htmlCode += "2028 1st round ML pick to <strong>Randall</strong> for <em>Cam Smith</em>, ";
+    htmlCode += "<em>Juan Brito</em>, and a 2027 1st round ML pick.</li>";
     htmlCode += '</ol>';
     return htmlCode;
 }
